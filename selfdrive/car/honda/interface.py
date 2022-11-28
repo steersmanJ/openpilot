69cbf9b4bbcf245edbcb45f946962e2696e6da52
steersmanJ <108046863+steersmanJ@users.noreply.github.com>
2022-11-28 13:16:33 +0900

    Update interface.py
    PiLOT LKAS 속도 하향

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -273,6 +273,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
       tire_stiffness_factor = 0.444
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.38], [0.11]]
+      ret.minSteerSpeed = 9. * CV.MPH_TO_MS
 
     elif candidate == CAR.PASSPORT:
       stop_and_go = False
